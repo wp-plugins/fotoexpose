5fa448b030a3b381b836c84c848089c593364847
montanan <montanan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 03:41:11 +0000

    tagging version 1.02
    git-svn-id: https://plugins.svn.wordpress.org/fotoexpose/tags/1.0.2@1160883 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/models/fotoexpose-model.php
+++ includes/models/fotoexpose-model.php
@@ -21,8 +21,12 @@ if( !class_exists( 'fotoexposeModel' ) ):
  			//start a domDocument to retive just image in the content
 			$dom = new domDocument; 
 			
+			$content = str_replace('<p>','', $this->content);
+			
+			$content = str_replace('</p>','', $content);
+			
 			//add html tags to vaildate the content
-			$dom->loadHTML('<html><body>'.$this->content.'</body></html>'); 
+			$dom->loadHTML('<html><body>'.$content.'</body></html>'); 
 			
 			$dom->preserveWhiteSpace = false; 
 			
@@ -30,56 +34,43 @@ if( !class_exists( 'fotoexposeModel' ) ):
 			$images = $dom->getElementsByTagName('img');
 			
 		
-				$image_list = array();
+			$image_list = array();
+			
+			foreach($images as $image) {
+			
+				//creating an array of the image to find in the post
+				preg_match('/wp-image-(?P<id>\d+)/',$image->getAttribute('class'),$matches);
 				
-				foreach($images as $image) {
-				
-					//creating an array of the image to find in the post
-					array_push($image_list, $image->getAttribute('src'));
-				
-				}
-				
-				$args = array(
-
-					'post_parent' => get_the_ID(),
-					
-					'post_type' => 'attachment'
-				
-				);
-				
-				$attachement_list = get_children( $args );
-				
-				$thumbs = '';
-				
-				if(is_array($attachement_list)) {
-				
-				foreach($attachement_list AS $attachment_id => $attachment) {
-				
+				array_push($image_list, $matches['id']);
+			
+			}
+			
+			$thumbs = '';
+			
+			if(is_array($image_list)) {
+			
+				foreach($image_list AS $list_key => $attachment_id) {
+		
 					$img_thumbs = wp_get_attachment_image_src( $attachment_id, 'thumbnail');
-					
+				
 					$img_medium = wp_get_attachment_image_src( $attachment_id, 'medium');
-					
+				
 					$img_large = wp_get_attachment_image_src( $attachment_id, 'large');
-					
-					if( in_array($img_medium[0],$image_list) ) {
-					
-						$thumbs .= '<img src="'.$img_thumbs[0].'" fe-full="'.$img_large[0].'" width="'.$img_thumbs[1].'" height="'.$img_thumbs[2].'" alt="galleryphoto" class="fe-thumbs">';
-
-						
-					}
-									
+				
+					$thumbs .= '<img src="'.$img_thumbs[0].'" fe-full="'.$img_large[0].'" width="'.$img_thumbs[1].'" height="'.$img_thumbs[2].'" alt="galleryphoto" class="fe-thumbs">';
+								
 				}
-				
+			
 				return $thumbs;
-				
+			
 			}
 			else {
-			
+		
 				return false;
-			
+		
 			}
 			
- 	}
+ 		}
  
 }   
     
